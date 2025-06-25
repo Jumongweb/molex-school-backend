e295cadd3bf9f53735b767542ed58ef8713ca601
Lawal Toheeb <lawaltoheeb36@gmail.com>
2025-06-25 18:40:44 +0100

    initial push

---
+++ src/main/java/molek_school/MolekSchoolApplication.java
@@ -0,0 +1,13 @@
+package molek_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolekSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolekSchoolApplication.class, args);
+	}
+
+}
